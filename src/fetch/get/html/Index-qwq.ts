cbaf048a42c3bda1d8bdbe3d7a78832efb9efdf6
Nworm <1574242600@qq.com>
2021-02-16 14:55:00 +0800

    fix: getIndexHtml(page)  page参数默认值错误

--- src/fetch/get/html/Index-qwq.ts
+++ src/fetch/get/html/Index-qwq.ts
@@ -7,7 +7,7 @@ import { fetchEX } from "../../fetch";
  * @param page number 页码 从1开始
  * @return string
  */
-export default (page = 0): Promise<string> => {
+export default (page = 1): Promise<string> => {
     return fetchEX(`/?page=${ page - 1 }`)
         .then((response) => response.text());
 };
