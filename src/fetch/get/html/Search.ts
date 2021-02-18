213e2a82839f46944ede1ec990e97d61725ba1ac
Nworm <1574242600@qq.com>
2021-02-18 12:32:52 +0800

    feat: 修改getSearchHtml 参数名称 getParams 改为 queryString

--- src/fetch/get/html/Search.ts
+++ src/fetch/get/html/Search.ts
@@ -4,12 +4,12 @@ import { IndexItemType } from "../../../types";
 /**
  * 获取搜索 html
  * @name getSearchHtml
- * @param getParams string get参数字符串
+ * @param queryString string get请求参数 字符串
  * @param type IndexItemType 条目类型
  * @return string
  */
 //todo 写测试
-export default (getParams: string, type: IndexItemType = "l"): Promise<string> => {
-    return fetchEX(`/?${ getParams }&inline_set=dm_${type}`)
+export default (queryString: string, type: IndexItemType = "l"): Promise<string> => {
+    return fetchEX(`/?${ queryString }&inline_set=dm_${type}`)
         .then((response) => response.text());
 };
