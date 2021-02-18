068689b937d92eb9bb67bbaa5debbb8f13fa73d9
Nworm <1574242600@qq.com>
2021-02-18 13:45:18 +0800

    feat: 完成 toAdvancedString()
    把 高级搜索参数 转换到 搜索字符串
    
    用于 获取搜索 html

---
+++ src/utils/__test__/searchParams/advanced.test.ts
@@ -0,0 +1,63 @@
+import { SearchParams } from "../../../types";
+import toAdvancedString, { 
+    toBetweenString, 
+    toDisableFilterString, 
+    toEnableString, 
+    toRatingString, 
+    toShowString
+} from "../../searchParams/advanced";
+
+const advanced: SearchParams["advanced"] = {
+    enable: {
+        name: true,
+        tags: true,
+        desc: true,
+        torr: true,
+        dt1: true,
+        dt2: true
+    },
+    show: {
+        torr: true,
+        delete: true
+    },
+    rating: 2,
+    between: [1, 2],
+    disableFilter: {
+        lang: true,
+        uploader: true,
+        tags: true
+    }
+}; 
+
+test("toAdvancedString()", () => {
+    expect(toAdvancedString(advanced))
+        .toBe("advsearch=1&f_sname=on&f_stags=on&f_sdesc=on&f_storr=on&f_sdt1=on&f_sdt2=on&f_sto=on&f_sh=on&f_sr=on&f_srdd=2&f_sp=on&f_spf=1&f_spt=2&f_sfl=on&f_sfu=on&f_sft=on");
+});
+
+describe("测试 toAdvancedString", () => {
+
+    it("toEnableString()", () => {
+        expect(toEnableString(advanced.enable))
+            .toBe("f_sname=on&f_stags=on&f_sdesc=on&f_storr=on&f_sdt1=on&f_sdt2=on");
+    });
+
+    it("toShowString()", () => {
+        expect(toShowString(advanced.show))
+            .toBe("f_sto=on&f_sh=on");
+    });
+
+    it("toRatingString()", () => {
+        expect(toRatingString(advanced.rating))
+            .toBe("f_sr=on&f_srdd=2");
+    });
+
+    it("toBetweenString()", () => {
+        expect(toBetweenString(advanced.between))
+            .toBe("f_sp=on&f_spf=1&f_spt=2");
+    });
+
+    it("toDisableFilterString()", () => {
+        expect(toDisableFilterString(advanced.disableFilter))
+            .toBe("f_sfl=on&f_sfu=on&f_sft=on");
+    });
+});
---
+++ src/utils/searchParams/advanced.ts
@@ -0,0 +1,55 @@
+import { SearchParams } from "../../types";
+
+export function toEnableString(enableParams: SearchParams["advanced"]["enable"]): string {
+    const queryArray = [];
+
+    if (enableParams.name === true) queryArray.push("f_sname=on"); 
+    if (enableParams.tags === true) queryArray.push("f_stags=on");
+    if (enableParams.desc === true) queryArray.push("f_sdesc=on");
+    if (enableParams.torr === true) queryArray.push("f_storr=on"); 
+    if (enableParams.dt1 === true) queryArray.push("f_sdt1=on");
+    if (enableParams.dt2 === true) queryArray.push("f_sdt2=on");
+
+    return queryArray.join("&");
+}
+
+export function toShowString(showParams: SearchParams["advanced"]["show"]): string {
+    const queryArray = [];
+
+    if (showParams.torr === true) queryArray.push("f_sto=on");
+    if (showParams.delete === true) queryArray.push("f_sh=on");
+
+    return queryArray.join("&");
+}
+
+export function toRatingString(rating: SearchParams["advanced"]["rating"]): string {
+    return `f_sr=on&f_srdd=${ rating }`;
+}
+
+export function toBetweenString(between: SearchParams["advanced"]["between"]): string {
+    return `f_sp=on&f_spf=${ between[0] }&f_spt=${ between[1] }`;
+}
+
+export function toDisableFilterString(DFParams: SearchParams["advanced"]["disableFilter"]): string {
+    const queryArray = [];
+
+    if (DFParams.lang === true) queryArray.push("f_sfl=on");
+    if (DFParams.uploader === true) queryArray.push("f_sfu=on");
+    if (DFParams.tags === true) queryArray.push("f_sft=on");
+
+    return queryArray.join("&");
+}
+
+export default function toAdvancedString(advancedParams: SearchParams["advanced"]): string {
+    const advancedArray = ["advsearch=1"];
+
+    if (typeof advancedParams.enable === "object") advancedArray.push(toEnableString(advancedParams.enable));
+    if (typeof advancedParams.show === "object") advancedArray.push(toShowString(advancedParams.show));
+    if (typeof advancedParams.rating === "number") advancedArray.push(toRatingString(advancedParams.rating));
+    if (advancedParams.between instanceof Array) advancedArray.push(toBetweenString(advancedParams.between));
+    if (typeof advancedParams.disableFilter === "object") advancedArray.push(toDisableFilterString(advancedParams.disableFilter));
+
+    return advancedArray
+        .filter((paramString) =>  paramString !== "")
+        .join("&");
+}
