49786472b0bf4b62fabfdcc84dff364c86414321
Nworm <1574242600@qq.com>
2021-02-21 17:49:08 +0800

    feat: 修改函数 toFileString() 新增参数 from

--- src/utils/searchParams/file.ts
+++ src/utils/searchParams/file.ts
@@ -1,12 +1,14 @@
 import { FileSearchParams } from "../../types";
 
 export default function toFileString(
+    from?: FileSearchParams["from"],
     similar?: FileSearchParams["similar"],
     covers?: FileSearchParams["covers"],
     exp?: FileSearchParams["exp"]
 ): string {
     const textArray = [];
 
+    if ( typeof from === "string" ) textArray.push("fs_from=1");
     if ( similar === true ) textArray.push("fs_similar=1");
     if ( covers === true ) textArray.push("fs_exp=1");
     if ( exp === true ) textArray.push( "fs_covers=1");
