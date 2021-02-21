6aef95209f17fe3477523e64c00cdcfe0def4c26
Nworm <1574242600@qq.com>
2021-02-21 19:55:20 +0800

    fix: toFileString() 测试用例错误

--- src/utils/__test__/searchParams/file.test.ts
+++ src/utils/__test__/searchParams/file.test.ts
@@ -1,6 +1,6 @@
 import toFileString from "../../searchParams/file";
 
 test("toFileString()", () => {
-    const text = toFileString(true, true, true);
-    expect(text).toBe("fs_similar=1&fs_exp=1&fs_covers=1");
+    const text = toFileString("qwq", true, true, true);
+    expect(text).toBe("fs_from=qwq&fs_similar=1&fs_exp=1&fs_covers=1");
 });
