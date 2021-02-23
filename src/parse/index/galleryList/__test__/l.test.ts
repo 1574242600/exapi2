235d44e07f898876cc849ec681c019dc92532dbf
Nworm <1574242600@qq.com>
2021-02-23 15:46:30 +0800

    feat: 调整目录结构, 完成 画廊列表类型L获取画廊标题 全部 画廊列表类型 L的测试 合并为 l.test.ts

---
+++ src/parse/index/galleryList/__test__/l.test.ts
@@ -0,0 +1,46 @@
+import fs from "fs";
+import cheerio from "cheerio";
+import beacon from "../l/beacon/index";
+import title from "../l/title/index";
+import type from "../l/type/index";
+
+describe("画廊列表类型 L", () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    ///@ts-ignore
+    const $ = cheerio.load(fs.readFileSync(global.__DATA_PATH__.html.index.l));
+
+    it("解析 画廊 id 和 token", () => {
+        const [ id, token ] = beacon(
+            $("table[class='itg gltc']>tbody>tr")
+                .eq(1)
+                .find("td")
+                .eq(2)
+        );
+    
+        expect( id > 0).toBe(true);
+        expect(/[a-z0-9]{10}/.test(token)).toBe(true);
+    });
+    
+    it("解析 画廊类型", () => {
+        const _type = type(
+            $("table[class='itg gltc']>tbody>tr")
+                .eq(1)
+                .find("td")
+                .eq(0)
+        );
+    
+        expect(_type).toBe("Image Set");
+    });
+    
+    it("解析 画廊标题", () => {
+        const _title = title(
+            $("table[class='itg gltc']>tbody>tr")
+                .eq(1)
+                .find("td")
+                .eq(2)
+        );
+    
+        expect(_title).toBe("[Artist] Shibekawa");
+    });
+
+});
---
+++ src/parse/index/galleryList/l/title/index.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+/**
+ * 获取画廊标题
+ * 
+ * @param $ Cheerio  标题 \<td\> 
+ * @return string 画廊标题
+ */
+export default ($: any): string => {
+    const title = $.find("a>div").eq(0).text();
+    return title;
+};
