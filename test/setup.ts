6dfcc20b3f1b9ca57ec4406c9cee34543f6111d2
Nworm <1574242600@qq.com>
2021-02-23 13:37:25 +0800

    feat: 完成 画廊列表类型L beacon 函数 测试

--- test/setup.ts
+++ test/setup.ts
@@ -1,3 +1,13 @@
 import { setHost } from "../src/fetch/config";
 
 setHost("http://localhost:50000");
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+///@ts-ignore
+global.__DATA_PATH__ = {
+    html: {
+        index: {
+            l: `${__dirname}/data/html/index/l/index.html`
+        }
+    }
+};
