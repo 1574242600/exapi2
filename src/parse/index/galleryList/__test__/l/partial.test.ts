5f7f460432fe6298ff7bdb980221e4febb07a682
Nworm <1574242600@qq.com>
2021-02-25 15:07:58 +0800

    feat: 重构 partial.test.ts

--- src/parse/index/galleryList/__test__/l/partial.test.ts
+++ src/parse/index/galleryList/__test__/l/partial.test.ts
@@ -15,115 +15,68 @@ describe("解析 画廊列表类型L HTML", () => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     ///@ts-ignore
     const $ = cheerio.load(fs.readFileSync(global.__DATA_PATH__.html.index.l));
+    const td = $("table[class='itg gltc']>tbody>tr")
+        .eq(1)
+        .children();
 
     it("解析 画廊 id 和 token", () => {
-        const [ id, token ] = beacon(
-            $("table[class='itg gltc']>tbody>tr")
-                .eq(1)
-                .find("td")
-                .eq(2)
-        );
-    
-        expect( id > 0).toBe(true);
+        const [id, token] = beacon(td.eq(2));
+
+        expect(id > 0).toBe(true);
         expect(/[a-z0-9]{10}/.test(token)).toBe(true);
     });
-    
+
     it("解析 画廊类型", () => {
-        const _type = type(
-            $("table[class='itg gltc']>tbody>tr")
-                .eq(1)
-                .find("td")
-                .eq(0)
-        );
-    
+        const _type = type(td.eq(0));
+
         expect(_type).toBe("Image Set");
     });
-    
+
     it("解析 画廊标题", () => {
-        const _title = title(
-            $("table[class='itg gltc']>tbody>tr")
-                .eq(1)
-                .find("td")
-                .eq(2)
-        );
-    
+        const _title = title(td.eq(2));
+
         expect(_title).toBe("[Artist] Shibekawa");
     });
 
     it("解析 画廊发布时间", () => {
-        const _published = published(
-            $("table[class='itg gltc']>tbody>tr")
-                .eq(1)
-                .find("td")
-                .eq(1)
-        );
-    
+        const _published = published(td.eq(1));
+
         expect(_published).toBe(1614050100);
     });
 
     it("解析 画廊评分", () => {
-        const _rating = rating(
-            $("table[class='itg gltc']>tbody>tr")
-                .eq(1)
-                .find("td")
-                .eq(1)
-        );
-    
+        const _rating = rating(td.eq(1));
+
         expect(_rating).toBe(4.5);
     });
 
     it("判断 画廊是否有种子", () => {
-        const _bt = bt(
-            $("table[class='itg gltc']>tbody>tr")
-                .eq(1)
-                .find("td")
-                .eq(1)
-        );
-    
+        const _bt = bt(td.eq(1));
+
         expect(_bt).toBe(true);
     });
 
     it("解析 画廊封面url", () => {
-        const _cover = cover(
-            $("table[class='itg gltc']>tbody>tr")
-                .eq(1)
-                .find("td")
-                .eq(1)
-        );
-    
+        const _cover = cover(td.eq(1));
+
         expect(_cover).toBe("https://ehgt.org/t/27/87/27875b5d97e37fb41533aa01e090f34d886407d5-897212-982-1200-jpg_250.jpg");
     });
 
     it("解析 画廊上传者", () => {
-        const _uploader = uploader(
-            $("table[class='itg gltc']>tbody>tr")
-                .eq(1)
-                .find("td")
-                .eq(3)
-        );
-    
+        const _uploader = uploader(td.eq(3));
+
         expect(_uploader).toBe("Godboli");
     });
 
     it("解析 画廊页数", () => {
-        const _pages = pages(
-            $("table[class='itg gltc']>tbody>tr")
-                .eq(1)
-                .find("td")
-                .eq(3)
-        );
-    
+        const _pages = pages(td.eq(3));
+
         expect(_pages).toBe(42);
     });
 
     it("解析 画廊部分标签", () => {
-        const _tags = tags(
-            $("table[class='itg gltc']>tbody>tr")
-                .eq(1)
-                .find("td")
-                .eq(2)
-        );
-        
+        const _tags = tags(td.eq(2));
+
         const namespaces = Object.keys(_tags);
 
         expect(namespaces.length > 0).toBe(true);
