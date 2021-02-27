09c84d663f45f596a36affcf2ebb2f4f03448f11
Nworm <1574242600@qq.com>
2021-02-27 13:38:43 +0800

    feat: 完成 filterCount 函数
    获取当前搜索条件下被过滤的画廊总数

--- src/parse/index/other/__test__/count.test.ts
+++ src/parse/index/other/__test__/count.test.ts
@@ -1,6 +1,8 @@
 import fs from "fs";
 import cheerio from "cheerio";
 import getResultCount from "../resultCount";
+import getFilterCount from "../filterCount";
+
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 ///@ts-ignore
 const $ = cheerio.load(fs.readFileSync(global.__DATA_PATH__.html.index.l));
@@ -16,3 +18,16 @@ describe("获取结果总数", () => {
         expect(total).toBe(0);
     });
 });
+
+describe("获取被过滤结果总数", () => {
+    it("正确的文本参数", () => {
+        const total = getFilterCount("Showing 536,350 results. Your filters excluded 1 gallery from this page");
+        expect(total).toBe(1);
+    });
+
+    it("错误的文本参数", () => {
+        const total = getFilterCount("S");
+        expect(total).toBe(0);
+    });
+});
+
---
+++ src/parse/index/other/filterCount.ts
@@ -0,0 +1,17 @@
+/**
+ * 获取当前搜索条件下被过滤的画廊总数
+ * 
+ * @param text string $("p[class='ip']").text()
+ * @return number 画廊总数
+ */
+export default (text: string): number => {
+    try {
+        const total = parseInt(
+            text.match(/excluded ([0-9]+) gallery from this page/)[1]
+        );
+
+        return total;
+    } catch {
+        return 0;
+    }
+};
