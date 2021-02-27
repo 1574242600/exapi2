60348172927589f7d5aaf41ad84de0c74cfb3703
Nworm <1574242600@qq.com>
2021-02-27 14:12:24 +0800

    feat: 完成 pageCount 函数
    获取当前搜索结果的总页数

--- src/parse/index/other/__test__/count.test.ts
+++ src/parse/index/other/__test__/count.test.ts
@@ -2,6 +2,7 @@ import fs from "fs";
 import cheerio from "cheerio";
 import getResultCount from "../resultCount";
 import getFilterCount from "../filterCount";
+import getPageCount from "../pageCount";
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 ///@ts-ignore
@@ -27,6 +28,18 @@ describe("获取被过滤结果总数", () => {
 
     it("错误的文本参数", () => {
         const total = getFilterCount("S");
+        expect(total).toBe(0);
+    });
+});
+
+describe("获取被过滤结果总数", () => {
+    it("正确的参数", () => {
+        const total = getPageCount($);
+        expect(total).toBe(24302);
+    });
+
+    it("错误的参数", () => {
+        const total = getPageCount(cheerio.load("<p></p>"));
         expect(total).toBe(0);
     });
 });
---
+++ src/parse/index/other/pageCount.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+/**
+ * 获取当前搜索结果的总页数
+ * 
+ * @param $ Cheerio.Root root
+ * @return number 总页数
+ */
+export default ($: any): number => {
+    try {
+        const text: string = $("table[class='ptt']>tbody>tr>td")
+            .eq(-2)
+            .text();
+
+        if (text.length === 0) return 0;
+        return parseInt(text);
+    } catch {
+        return 0;
+    }
+};
