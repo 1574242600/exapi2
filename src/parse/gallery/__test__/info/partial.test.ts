b1ad7aaab7632b2b993c0b408b8fc1ce97631760
Nworm <1574242600@qq.com>
2021-06-05 16:31:36 +0800

    🚚 ( parse/ ):  重命名所有 partial/ 为 meta/

--- src/parse/gallery/__test__/info/partial.test.ts
+++ src/parse/gallery/__test__/info/partial.test.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 import cheerio from "cheerio";
-import * as Partial from "../../info/partial/index";
+import * as Meta from "../../meta/index";
 
 describe("解析画廊html", () => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -8,12 +8,12 @@ describe("解析画廊html", () => {
     const $ = cheerio.load(fs.readFileSync(global.__DATA_PATH__.html.gallery.m));
     
     it("获取 画廊封面url", () => {
-        const coverUrl = Partial.getCoverUrl($("#gleft"));
+        const coverUrl = Meta.getCoverUrl($("#gleft"));
         expect(coverUrl.includes("https://ehgt.org/")).toBe(true);
     });
 
     it("获取 画廊标题", () => {
-        const title = Partial.getTitle($("#gd2"));
+        const title = Meta.getTitle($("#gd2"));
 
         expect(title[0].includes("Asaba Hiromu")).toBe(true);
         expect(title[1].includes("あさば☆ひろむ")).toBe(true);
