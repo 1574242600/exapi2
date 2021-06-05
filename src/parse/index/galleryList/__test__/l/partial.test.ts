ec31888d9a8f8f5fda7fd4e7c3f853f98103a898
Nworm <1574242600@qq.com>
2021-06-05 16:31:36 +0800

    🎨 ( parse/ ):  重命名所有 partial/ 为 meta/

--- src/parse/index/galleryList/__test__/l/partial.test.ts
+++ src/parse/index/galleryList/__test__/l/partial.test.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 import cheerio from "cheerio";
-import * as Partial from "../../l/partial/index";
+import * as Meta from "../../l/meta/index";
 
 describe("解析画廊条目Html 类型L", () => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -11,62 +11,62 @@ describe("解析画廊条目Html 类型L", () => {
         .children();
 
     it("获取画廊 id 和 token", () => {
-        const [id, token] = Partial.getBeacon(td.eq(2));
+        const [id, token] = Meta.getBeacon(td.eq(2));
 
         expect(id > 0).toBe(true);
         expect(/[a-z0-9]{10}/.test(token)).toBe(true);
     });
 
     it("获取画廊类型", () => {
-        const type = Partial.getType(td.eq(0));
+        const type = Meta.getType(td.eq(0));
 
         expect(type).toBe("Image Set");
     });
 
     it("获取画廊标题", () => {
-        const title = Partial.getTitle(td.eq(2));
+        const title = Meta.getTitle(td.eq(2));
 
         expect(title).toBe("[Artist] Shibekawa");
     });
 
     it("获取画廊发布时间", () => {
-        const published = Partial.getPublished(td.eq(1));
+        const published = Meta.getPublished(td.eq(1));
 
         expect(published).toBe(1614050100);
     });
 
     it("获取画廊评分", () => {
-        const rating = Partial.getRating(td.eq(1));
+        const rating = Meta.getRating(td.eq(1));
 
         expect(rating).toBe(4.5);
     });
 
     it("判断画廊是否有种子", () => {
-        const bt = Partial.isHaveBt(td.eq(1));
+        const bt = Meta.isHaveBt(td.eq(1));
 
         expect(bt).toBe(true);
     });
 
     it("获取画廊封面url", () => {
-        const coverUrl = Partial.getCoverUrl(td.eq(1));
+        const coverUrl = Meta.getCoverUrl(td.eq(1));
 
         expect(coverUrl.includes("https://ehgt.org/")).toBe(true);
     });
 
     it("获取画廊上传者", () => {
-        const uploader = Partial.getUploader(td.eq(3));
+        const uploader = Meta.getUploader(td.eq(3));
 
         expect(uploader).toBe("Godboli");
     });
 
     it("获取画廊页数", () => {
-        const pages = Partial.getPages(td.eq(3));
+        const pages = Meta.getPages(td.eq(3));
 
         expect(pages).toBe(42);
     });
 
     it("获取画廊部分标签", () => {
-        const tags = Partial.getTags(td.eq(2));
+        const tags = Meta.getTags(td.eq(2));
 
         const namespaces = Object.keys(tags);
 
