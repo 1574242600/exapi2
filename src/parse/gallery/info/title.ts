7e074d6906a28f2a19ab57b230a5f2c79a63a9ff
Nworm <1574242600@qq.com>
2021-06-12 14:09:58 +0800

    ♻️ ( parse/gallery/info/title.ts ): 重构 getTitle()

--- src/parse/gallery/info/title.ts
+++ src/parse/gallery/info/title.ts
@@ -8,18 +8,24 @@
  * @return [string, string] | [string, null] 画廊标题 [ 默认标题, 日文标题 (如果有) ]
  */
 export default function getTitle(gd2: any): [string, string] | [string, null] {
-    const defaultTitle: string = gd2.find("#gn").text();
+    return [
+        getDefaultTitle(gd2),
+        getJPTitle(gd2)
+    ];
+}
 
+export function getDefaultTitle(gd2: any): string {
+    const defaultTitle = gd2.find("#gn").text();
+    
+    return defaultTitle;
+}
+
+export function getJPTitle(gd2: any): string {
     try {
         const JPTitle: string = gd2.find("#gj").text();
-        const isHaveJPT = (<string>JPTitle).length > 0;
 
-        return [
-            defaultTitle, 
-            isHaveJPT ? JPTitle : null
-        ];
-        
+        return (<string>JPTitle).length > 0 ? JPTitle : null;
     } catch {
-        return [defaultTitle, null];
+        return null;
     }
 }
