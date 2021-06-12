49842dd33c3db26b43c7b2596da32977787ee6d8
Nworm <1574242600@qq.com>
2021-06-12 11:55:58 +0800

    ✅ ( parse/gallery ): 新增 getType() 测试

--- src/parse/gallery/__test__/info/partial.test.ts
+++ src/parse/gallery/__test__/info/partial.test.ts
@@ -6,7 +6,7 @@ describe("解析画廊html", () => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     ///@ts-ignore
     const $ = cheerio.load(fs.readFileSync(global.__DATA_PATH__.html.gallery.m));
-    
+
     it("获取 画廊封面url", () => {
         const coverUrl = Meta.getCoverUrl($("#gleft"));
         expect(coverUrl.includes("https://ehgt.org/")).toBe(true);
@@ -17,5 +17,11 @@ describe("解析画廊html", () => {
 
         expect(title[0].includes("Asaba Hiromu")).toBe(true);
         expect(title[1].includes("あさば☆ひろむ")).toBe(true);
+    });
+
+    it("获取 画廊类型", () => {
+        const type = Meta.getType($("#gd3"));
+
+        expect(type).toBe("Non-H");
     });
 });
