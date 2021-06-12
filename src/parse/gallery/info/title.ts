8fa38d5ab2069fb06b00675d41ef8b281f894a00
Nworm <1574242600@qq.com>
2021-06-12 17:36:20 +0800

    💡 ( parse/gallery/info/title.ts ): 修复注释中错误

--- src/parse/gallery/info/title.ts
+++ src/parse/gallery/info/title.ts
@@ -4,7 +4,7 @@
 /**
  * 解析画廊html: 获取画廊标题
  * @name getTitle
- * @param gleft Cheerio \<div id=\"gd2\"\> 
+ * @param gd2 Cheerio \<div id=\"gd2\"\> 
  * @return [string, string] | [string, null] 画廊标题 [ 默认标题, 日文标题 (如果有) ]
  */
 export default function getTitle(gd2: any): [string, string] | [string, null] {
