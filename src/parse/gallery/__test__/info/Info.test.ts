166e48011b26848128e471ac0e14b5f8c711c5f5
Nworm <1574242600@qq.com>
2021-07-02 18:17:13 +0800

    ✅ ( parse/gallery/info ): 新增测试
    获取 默认标题
    获取 日文标题

--- src/parse/gallery/__test__/info/Info.test.ts
+++ src/parse/gallery/__test__/info/Info.test.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 import cheerio from "cheerio";
 import * as Info from "../../info/index";
+import { getDefaultTitle, getJPTitle } from "../../info/titles";
 
 describe("解析画廊html", () => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -12,11 +13,32 @@ describe("解析画廊html", () => {
         expect(result.includes("https://ehgt.org/")).toBe(true);
     });
 
-    it("获取 画廊标题", () => {
-        const title = Info.getTitle($("#gd2"));
+    describe("画廊标题", () => {
+        const gd2 = $("#gd2");
+        const [ defaultT, JPT ] = [
+            "Asaba Hiromu",
+            "あさば☆ひろむ"
+        ];
 
-        expect(title[0].includes("Asaba Hiromu")).toBe(true);
-        expect(title[1].includes("あさば☆ひろむ")).toBe(true);
+        it("获取 画廊标题", () => {
+            const results = Info.getTitles(gd2);
+    
+            expect(results[0] === defaultT).toBe(true);
+            expect(results[1] === JPT).toBe(true);
+        });
+
+        it("获取 默认标题", () => {
+            const result = getDefaultTitle(gd2);
+    
+            expect(result === defaultT).toBe(true);
+        });
+
+        it("获取 日文标题", () => {
+            const result = getJPTitle(gd2);
+    
+            expect(result === JPT).toBe(true);
+        });
+       
     });
 
     it("获取 画廊类型", () => {
