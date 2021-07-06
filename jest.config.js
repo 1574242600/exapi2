8317eeb052648bcff21132270f5c0b3f1f9cafaf
Nworm <1574242600@qq.com>
2021-07-06 14:57:09 +0800

    🔧 ( jest ):  修改 jest 配置以修复 Could not locate module

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,13 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require("./tsconfig.json");
+
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
     testPathIgnorePatterns: ["node_modules"],
     setupFiles: ["<rootDir>/test/setup.ts"],
     globalSetup: "<rootDir>/test/global/setup.ts",
-    globalTeardown: "<rootDir>/test/global/teardown.ts"
+    globalTeardown: "<rootDir>/test/global/teardown.ts",
+    modulePaths: [`<rootDir>/${ compilerOptions.baseUrl }`],
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths)
 };
